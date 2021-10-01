89f350cfdbc8afce7f35a824b915db36d0918463
irah0001 <irah0001@synchronoss.com>
2021-09-30 22:16:50 -0700

    optimzed with hashmap if duplicate elements are present and added test cases

--- Algo/src/zoomInt/TestSumSwap.java
+++ Algo/src/zoomInt/TestSumSwap.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class TestSumSwap {
@@ -30,14 +32,23 @@ public class TestSumSwap {
 
     private static int[] findSwapValues(int[] firstArray, int[] secondArray) {
         int elementInFirstArray = 0;
+        Map<Integer, Boolean> visitedFirstArray = new HashMap<>();
 
         if (returnTrueIfSumsAreEqual(firstArray, secondArray)) return new int[]{};
 
         for (int i = 0; i < firstArray.length; i++) {
             for (int j = 0; j < secondArray.length; j++) {
+
                 int[] copyFirst = Arrays.copyOf(firstArray, firstArray.length);
+                if(!visitedFirstArray.containsKey(copyFirst[i])) {
+                    visitedFirstArray.put(copyFirst[i], true);
+                } else {
+                    i++;
+                }
+
                 elementInFirstArray = copyFirst[i];
                 int[] copyOfSecond = Arrays.copyOf(secondArray, secondArray.length);
+
                 copyFirst[i] = copyOfSecond[j];
                 copyOfSecond[j] = elementInFirstArray;
                 if (returnTrueIfSumsAreEqual(copyFirst, copyOfSecond)) {
@@ -50,15 +61,26 @@ public class TestSumSwap {
 /// code goes here
 
     @Test
-    public void test1() {
+    public void test_when_equal_sums_found() {
 
         int[] firstArray = {4, 1, 2, 1, 1, 2}; // 11 /// 11+3 = 14
         int[] secondArray = {3, 6, 3, 5};   // 17 //-3 = 14
-
         Assertions.assertArrayEquals(new int[]{2, 5}, findSwapValues(firstArray, secondArray));
+    }
 
+    @Test
+    public void test_when_empty_array() {
+        Assertions.assertArrayEquals(new int[]{}, findSwapValues(new int[]{}, new int[]{}));
+    }
+
+    @Test
+    public void test_when_equal_sums_not_found() {
+
+        int[] firstArray = {4,20};
+        int[] secondArray = {3, 6, 3};
+        Assertions.assertArrayEquals(new int[]{}, findSwapValues(firstArray, secondArray));
     }
 
 //Jin.li@zoom.us
 
-            }
+}
