a25d5798e5030303b651a1fff0f1e7ec86c4843c
irah0001 <irah0001@synchronoss.com>
2021-10-20 19:14:15 -0700

    revere a stack

---
+++ Algo/src/recursion/ReverseAStack.java
@@ -0,0 +1,48 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+public class ReverseAStack {
+
+
+    private void reverse(Stack<Integer> st) {
+
+        if (st.isEmpty()) return;
+
+        int element = st.pop();
+
+        reverse(st);
+
+        insertAtBottom(st, element);
+    }
+
+
+    public static void insertAtBottom(Stack<Integer> st, int element) {
+        if (st.isEmpty()) {
+            st.push(element);
+            return;
+        }
+
+        int temp = st.pop();
+        insertAtBottom(st, element);
+        st.push(temp);
+    }
+
+
+    @Test
+    public void test() {
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        st.push(10);
+        st.push(15);
+        st.push(20);
+
+        System.out.println(st.toString());
+
+        reverse(st);
+        System.out.println(st.toString());
+
+    }
+}
